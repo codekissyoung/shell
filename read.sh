7b72c528d3542dd38956fce9624d1cf8e2ff70b1
codekissyoung <cky951010@163.com>
2017-06-29 18:30:14 +0800

    update blog

--- read.sh
+++ read.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# 不需要回车回车
+read -n 2 var
+echo "enter $var"
+
+# 以无回显的方式读取密码
+read -s var2
+echo "var2 : $var2";
+
+# -p 以特定格式输出 -t 限定多少秒内输入
 read -t 3 -p "do you love me ? ( Y / N ) : " answer
 
 if [ -n "$answer" ];then
