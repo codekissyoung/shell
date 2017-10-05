474fbfaeba929452c7ef74a33ba735dcda6b47f4
caokaiyan <cky951010@163.com>
2017-10-05 13:32:23 +0800

    又开始学习shell了

---
+++ main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 输入与输出
+echo -n "Hello ";
+echo "Shell ! ";
+echo "";
+
+# 重要的环境变量
+echo "常用的环境变量: ";
+echo "HOME     : ${HOME}";
+echo "UID      : ${UID}";
+echo "USER     : ${USER}";
+
+
+
+
+
+
+
