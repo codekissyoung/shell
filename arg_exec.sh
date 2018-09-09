de64a640be6bbba056a465e3ac80dc573edef967
codekissyoung <cky951010@163.com>
2018-09-10 01:49:52 +0800

    update

--- arg_exec.sh
+++ arg_exec.sh
@@ -2,14 +2,14 @@
 
 while [ -n "$1" ] ;do
 	case "$1" in
-	first)
-		echo "first 参数";;
-	second)
-		echo "second 参数";;
-	third)
-		echo "third 参数";;
-	*)
-		echo "处理其他参数";;
+		first)
+			echo "first 参数";;
+		second)
+			echo "second 参数";;
+		third)
+			echo "third 参数";;
+		*)
+			echo "处理其他参数";;
 	esac
 	shift
 done
--- auto_input.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-read -p "Enter your name:" name;
-read -p "Enter your age:" age;
-echo "your name is $name , you are $age years old"
