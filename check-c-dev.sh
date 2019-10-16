510efe3ac4def50fbca2c6bed1d4da76bd95dc51
codekissyoung <1162097842@qq.com>
2019-10-16 18:18:06 +0800

    upgrade

--- check-c-dev.sh
+++ check-c-dev.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
+
 echo "内核版本:"
-uname -a              
+uname -a; echo;                             # 检测 linux 内核版本
 
 echo "发行版本:";
-lsb_release -a
+lsb_release -a; echo;                       # 查看发行版本
 
 echo "gcc版本:"
-gcc -v
+gcc -v  | grep "gcc version"; echo;         # 检查 gcc 版本
 
 echo "gdb版本:";
-gdb -v
+gdb -v  | grep -i "GNU gdb";echo;           # 检查 gdb 版本
 
 echo "make版本:";
-make -v                 
+make -v | grep -i make;echo;                # 检查 make 版本
 
 echo "autoconf版本:";
-autoconf --version      
+autoconf --version | grep autoconf;echo;    # 检查 autoconf 版本
 
 echo "automake版本:";
-automake --version
+automake --version | grep automake;echo;    # 检查 automake 版本
 
 echo "libtoolize版本";
-libtoolize --version
+libtoolize --version | grep automake;echo;  # 检查 libtool 版本
 
-echo "sudo apt-get install gcc gdb make autoconf automake libtool build-essential";
+echo "cmake版本";
+cmake --version ;echo;                      # 检查 cmake 版本
+
+
--- ubuntu.sh
+++ ubuntu.sh
@@ -30,14 +30,22 @@ sudo apt-get install zhcon
 sudo apt-get install aptitude
 
 # 必备软件
+sudo apt-get install gcc gdb make autoconf automake libtool build-essential flex bison cmake git tree
 sudo apt-get install vim
+sudo update-alternatives --config editor                # 默认编辑设置为vim
 sudo apt-get install git
-sudo apt-get install rar unrar
-sudo apt-get install p7zip
+git config --global core.quotepath false                # git中文正确显示
+git config --global core.editor vim                     # 默认编辑器 vim
+git config --global user.name "codekissyoung"           # git 用户名
+git config --global user.email "1162097842@qq.com"      # git email
+
+sudo apt-get install rar unrar                  # 安装rar解压工具, unrar x test.rar 解压到当前文件夹
+sudo apt-get install p7zip                      # .7z 压缩的解压工具
 sudo apt-get install zsh
-sudo apt-get install bless
+sudo apt-get install bless                      # 安装二进制查看与编辑器
 sudo apt-get install tmux
 sudo apt-get install openssh-server
+sudo apt-get install lnav                       # 安装终端看访问日志的神器 lnav观看 [服务器端]
 sudo apt-get install bash-builtins bash-completion bash-doc bash-static
 
 # 安装 c++ 开发环境
